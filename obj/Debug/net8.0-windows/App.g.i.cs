6c7c89baedb271c4c410115c708022c4c56a8aaf
Shahar Max <shaharmax123@gmail.com>
2024-12-26 13:53:46 +0200

    added words and slight refactor

--- obj/Debug/net8.0-windows/App.g.i.cs
+++ obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace WpfApp1 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         public void InitializeComponent() {
             
             #line 6 "..\..\..\App.xaml"
@@ -66,7 +66,7 @@ namespace WpfApp1 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         public static void Main() {
             WpfApp1.App app = new WpfApp1.App();
             app.InitializeComponent();
